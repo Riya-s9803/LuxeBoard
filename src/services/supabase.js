9cb6c6795c05233642c3d3b5b02cb96e4dfdf86e
Riya-s9803 <123238786+Riya-s9803@users.noreply.github.com>
2026-03-23 22:35:42 +0530

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,11 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+export const supabaseUrl = "https://whfxnicgwsmoypdqrnyi.supabase.co";
 const supabaseKey =
-  import.meta.env.VITE_SUPABASE_ANON_KEY;
-console.log("ENV:",import.meta.env);
-console.log("URL:",import.meta.env.VITE_SUPABASE_URL;
-export const supabase = createClient(supabaseUrl, supabaseKey);
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoZnhuaWNnd3Ntb3lwZHFybnlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNjk0MTAsImV4cCI6MjA4OTg0NTQxMH0.-5ilw8AL1rKCd-3jbhxmu77QzoD0e3Xck79-wU3B-Gc";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
